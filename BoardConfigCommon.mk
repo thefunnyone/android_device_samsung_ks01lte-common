f3e6320e2f8d4b4a5f1060f9accc2e69a6b6cecc
thefunnyone <thefunnyone@users.noreply.github.com>
2015-07-05 16:22:49 +0530

    Update BoardConfigCommon.mk
    add more ks01ltes

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,7 +15,7 @@
 # inherit from common msm8974
 -include device/samsung/msm8974-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/samsung/hlte-common
+LOCAL_PATH := device/samsung/ks01lte-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
@@ -31,8 +31,8 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 TARGET_KERNEL_CONFIG := msm8974_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_eur_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/hlte
+TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_ks01lte_eur_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/ks01lte
 
 # Audio
 QCOM_CSDCLIENT_ENABLED := false
@@ -41,7 +41,7 @@ AUDIO_FEATURE_ENABLED_HWDEP_CAL := true
 AUDIO_FEATURE_ENABLED_LOW_LATENCY_CAPTURE := true
 
 # Bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/vnd_hlte.txt
+BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/vnd_ks01lte.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 BOARD_HAVE_BLUETOOTH_BCM := true
@@ -51,11 +51,11 @@ TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # CMHW
-BOARD_HARDWARE_CLASS += device/samsung/hlte-common/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/ks01lte-common/cmhw
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
 
 # RIL
-BOARD_RIL_CLASS := ../../../device/samsung/hlte-common/ril
+BOARD_RIL_CLASS := ../../../device/samsung/ks01lte-common/ril
 
 # Graphics
 TARGET_HAVE_NEW_GRALLOC := true
@@ -82,7 +82,7 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_UNION += macloader.te
-BOARD_SEPOLICY_DIRS += device/samsung/hlte-common/sepolicy
+BOARD_SEPOLICY_DIRS += device/samsung/ks01lte-common/sepolicy
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
